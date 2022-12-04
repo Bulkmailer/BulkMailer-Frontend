b5e39796f18cf3f8d7e8d320e1fcb4add509eaac
v <v>
2022-12-04 23:50:56 +0530

    fixed a bug of empty input

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -10,15 +10,18 @@ const list = useSelector((state)=>state.todoReducer.list);
 return(<>
 <div className='container'>
 <h1>ToDo List</h1>
+<form>
 <div className='input'>
     <input type='text' placeholder='Write your Todos here' id='inputtodo' 
     value={todo} 
+    required
     onChange={(e) => setTodo(e.target.value)}
     ></input>
-    <button id='plus' 
+    <button id='plus' type='submit'
     onClick={()=>dispatch(Add_Todo(todo) , setTodo(''))}
     >+</button>
 </div>
+</form>
 {
     list.map((e)=>{
         return(
--- src/reducers/todoreducer.js
+++ src/reducers/todoreducer.js
@@ -5,6 +5,7 @@ const todoReducer =(state=initialData , action)=>{
    switch(action.type){
     case "Add_Todo" :
         const { id, data } = action.payload;
+        if(data){
         return{
             ...state,
             list:[
@@ -15,6 +16,7 @@ const todoReducer =(state=initialData , action)=>{
                 }
             ]
         }
+    }
         case "Remove_Todo" :
        const deletedlist= state.list.filter((e) => e.id != action.id )
         return{
