26f142149f1fcacad8d5d6cf494af8430d62e76c
v <v>
2023-01-24 14:58:03 +0530

    fixed api base url

--- src/redux/apis.jsx
+++ src/redux/apis.jsx
@@ -1,4 +1,4 @@
 import axios from 'axios';
 export default axios.create({
-    baseURL:"http://bulkmailersi.centralindia.cloudapp.azure.com/"
+    baseURL:"https://bulkmailersi.centralindia.cloudapp.azure.com/"
 })
