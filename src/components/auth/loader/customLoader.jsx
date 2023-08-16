d418a00386a24efdc4ebecd4b95cc1dae54bc355
personal <suhaillahmadd0@gmail.com>
2023-08-16 22:48:45 +0530

    Added custom loader

---
+++ src/components/auth/loader/customLoader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./customLoader.css";
+
+function CustomLoader() {
+  return (
+    <>
+      <div className="shapes-9-container">
+        <div className="shapes-9">
+          {/* Empty, the loader is created purely with CSS */}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CustomLoader;
