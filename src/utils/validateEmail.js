9e949754b3e75a0e96fdef78e4592ef37503f792
rahul palamarthi <rahulpalamarthi@gmail.com>
2023-10-04 14:02:54 +0530

    Fix: Signup form email error message is below input

---
+++ src/utils/validateEmail.js
@@ -0,0 +1,7 @@
+export function validateEmail(email) {
+	const regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+	if (!email || regex.test(email) === false) {
+		return false;
+	}
+	return true;
+}
